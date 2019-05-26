ba583817212eb5cb1ea79271882691308f05669e
Markus Suzuki <markus_suzuki@hotmail.com>
2019-05-26 12:19:33 -0400

    Render Companies,Cars to home page

--- Project_Chapter4_ItemCatalog/app.py
+++ Project_Chapter4_ItemCatalog/app.py
@@ -12,49 +12,28 @@ session = DBSession()
 
 #companies = session.query(Company).all()
 #cars = session.query(Cars).all()
-
 #for car in cars:
 #    print(car.car_name, car.company.name)
-
 #Testing Calling outside function
 #hi = hello()
 #print(hi)
 
 
-
-
-#app = Flask(__name__)
-
-# http://localhost:8000/
-#@app.route("/")
-#def hello():
-#    return "Hello World!"
-
-
-# @app.route('/')
-# @app.route('/restaurant/')
-# def showRestaurants():
-#     restaurants = session.query(Restaurant).all()
-#     # return "This page will show all my restaurants"
-# return render_template('restaurants.html', restaurants=restaurants)
+app = Flask(__name__)
 
 
 
 
-# http://localhost:8000/catalog/Snowboarding/items
+@app.route('/')
+@app.route('/companies/')
+def show():
+    companies = session.query(Company).all()
+    cars = session.query(Cars).all()
+    return render_template('companies.html', companies=companies, cars=cars)
 
 
-# http://localhost:8000/catalog/Snowboarding/Snowboard
 
 
-# http://localhost:8000/catalog/Snowboard/edit (logged in)
-
-
-# http://localhost:8000/catalog/Snowboard/delete (logged in)
-
-
-# http://localhost:8000/catalog.json
-
-#if __name__ == '__main__':
-#    app.debug = True
-#    app.run(host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+   app.debug = True
+   app.run(host='0.0.0.0', port=8000)
